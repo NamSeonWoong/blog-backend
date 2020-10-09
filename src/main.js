572d75ec399526f9ba9dfdde8675011372d00e68
남선웅 <wun1107@gmail.com>
2020-10-09 22:30:08 +0900

    mongoose로 서버, DB 연결

---
+++ src/main.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const api = require('./api');
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false , useUnifiedTopology:true })
+  .then(() => {
+    console.log('Mongo DB 연결');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log('Listen %d', port);
+});
