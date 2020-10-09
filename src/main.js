968892f9140e01d4788032737f9013f50e08ca38
남선웅 <wun1107@gmail.com>
2020-10-09 23:38:42 +0900

    mongoDB REST API 개발완료

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
-require('dotenv').config();
+require ('dotenv').config();
 
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-
-const api = require('./api');
-
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import api from './api';
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false , useUnifiedTopology:true })
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology:true  })
   .then(() => {
     console.log('Mongo DB 연결');
   })
@@ -25,7 +23,6 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
-
 const port = PORT || 4000;
 
 app.listen(port, () => {
